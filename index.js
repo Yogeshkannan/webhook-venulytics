d68b984ab3e8a4d0055291732ca7fdba3322cc80
Yogesh <yogesh@10decoders.in>
2018-04-25 18:11:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const moment = require('moment');
+
+const bookingService = require('./service/bookingService');
+
+const server = express();
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.use(bodyParser.json());
+
+server.post('/venulytics', (req, res) => {
+
+    var token = "dGVzdCB0ZXN0OnRlc3RAZ21haWwuY29tOig4ODgpIDg4OC04ODg4";
+    var intentName = req.queryResult.intent.displayName;
+    var bookedVenueNumber, bookedTableId;
+
+    if(intentName === 'UserProvidesVenueName') {
+      var venueNameSlot = req.body.result.parameters.venueName;
+      if(venueNameSlot) {
+        const venueName = venueNameSlot;
+        bookingService.getVenueDetails(venueName, function (venueNumber) {
+            if (venueNumber != "ERROR") {
+                bookedVenueNumber = venueNumber;
+                var dataToSend = "To book a reservation in " + venueName + ", Please choose the table number";
+                return res.json({
+                    speech: dataToSend,
+                    displayText: dataToSend,
+                    source: 'venulytics'
+                });
+            } else {
+                return res.json({
+                    speech: "Invalid venue name",
+                    displayText: "Invalid venue name",
+                    source: 'venulytics'
+                });
+            }
+        })
+      } else {
+        return res.json({
+            speech: 'please, tell me the venue name',
+            displayText: 'please, tell me the venue name',
+            source: 'venulytics'
+        });
+      }
+    } else if(intentName === 'UserProvidesTableName') {
+      const tableNumberSlot = req.body.result.parameters.tableNumber;
+      if (tableNumberSlot) {
+          const tableNumber = tableNumberSlot;
+          bookingService.getTableDetails(tableNumber, bookedVenueNumber, function (tableName, tableId) {
+              var dataToSend;
+              if (tableName.status != "ERROR") {
+                  bookedTableId = tableId;
+                  dataToSend = "Do you want to confirm?";
+              } else {
+                  dataToSend = "choosing table not available";
+              }
+              return res.json({
+                  speech: dataToSend,
+                  displayText: dataToSend,
+                  source: 'venulytics'
+              });
+          })
+      } else {
+        return res.json({
+            speech: 'please, tell me the table number',
+            displayText: 'please, tell me the table number',
+            source: 'venulytics'
+        });
+      }
+
+    } else if(intentName === 'UserConfirms') {
+      const confirmationSlot = req.body.result.parameters.yesOrNo;
+      if(confirmationSlot) {
+        const yesOrNo = ConfirmationSlot;
+        if(yesOrNo === 'yes') {
+          bookingService.orderConfirmation(bookedVenueNumber, bookedTableId, function (tableName) {
+              var dataToSend;
+              if ((tableName.status != 404) && (tableName.status != 500)) {
+                  dataToSend = "Your " + tableName.order.orderItems[0].name + "is booked successfully!";
+              } else {
+                  dataToSend = "Sorry, your order is not booked";
+              }
+              return res.json({
+                  speech: dataToSend,
+                  displayText: dataToSend,
+                  source: 'venulytics'
+              });
+          })
+        } else {
+          return res.json({
+              speech: 'Thanks for your using our booking service',
+              displayText: 'Thanks for your using our booking service',
+              source: 'venulytics'
+          });
+        }
+      } else {
+        return res.json({
+            speech: 'Please, confirm your order',
+            displayText: 'Please, confirm your order',
+            source: 'venulytics'
+        });
+      }
+    } else {
+        throw new Error('Invalid intent');
+    }
+});
+
+server.listen((process.env.PORT || 8000), () => {
+    console.log("Server is up and running...");
+});
---
+++ service/bookingService.js
@@ -0,0 +1,109 @@
+function getVenueDetails(venueName, callback) {
+    request.get(venueDetailsRequest(venueName), function (error, response, body) {
+        var d = JSON.parse(body);
+        var result = d.venues;
+        if (result.length > 0) {
+            callback(result[0].id);
+        } else {
+            callback("ERROR");
+        }
+    })
+}
+
+function venueDetailsRequest(venueName) {
+    var token = "dGVzdCB0ZXN0OnRlc3RAZ21haWwuY29tOig4ODgpIDg4OC04ODg4";
+    var options = {
+        url: "http://dev.api.venuelytics.com/WebServices/rsapi/v1//venues/q?lat=&lng=&dist=20000&search=" + venueName,
+        headers: {
+            'Authorization': 'Anonymous ' + token
+        }
+    };
+    return options;
+}
+
+function getTableDetails(tableNumber, bookedVenueNumber, callback) {
+    request.get(tableDetailsRequest(tableNumber, bookedVenueNumber), function (error, response, body) {
+        var result = JSON.parse(body);
+        if (result) {
+            for (var i = 0; i < result.length; i++) {
+                var element = result[i].elements;
+                for (var j = 0; j < element.length; j++) {
+                    if (tableNumber === element[j].name.toLowerCase()) {
+                        callback(element[j].name, element[j].id);
+                    }
+                }
+            }
+        } else {
+            callback("ERROR");
+        }
+    })
+}
+
+function tableDetailsRequest(tableNumber, bookedVenueNumber) {
+    var token = "dGVzdCB0ZXN0OnRlc3RAZ21haWwuY29tOig4ODgpIDg4OC04ODg4";
+    var options = {
+        url: 'http://dev.api.venuelytics.com/WebServices/rsapi/v1/venuemap/' + bookedVenueNumber,
+        headers: {
+            'Authorization': 'Anonymous ' + token
+        }
+    };
+    return options;
+}
+
+function orderConfirmation(bookedVenueNumber, bookedTableId, callback) {
+    request.post(bookYourOrder(bookedVenueNumber, bookedTableId), function (error, response, body) {
+        var result = JSON.parse(body);
+        if (result) {
+            callback(result);
+        } else {
+            callback("ERROR");
+        }
+    })
+}
+
+function bookYourOrder(bookedVenueNumber, bookedTableId) {
+    var ENDPOINT = 'http://dev.api.venuelytics.com/WebServices/rsapi/v1/vas/';
+    var token = "dGVzdCB0ZXN0OnRlc3RAZ21haWwuY29tOig4ODgpIDg4OC04ODg4";
+    var options = {
+        method: 'POST',
+        uri: ENDPOINT + bookedVenueNumber + '/orders',
+        body: JSON.stringify({
+            "serviceType": "Bottle",
+            "venueNumber": bookedVenueNumber,
+            "reason": "Birthday Party",
+            "contactNumber": "(888) 888-8888",
+            "contactEmail": "test@gmail.com",
+            "contactZipcode": "23987298",
+            "noOfGuests": 2,
+            "noOfMaleGuests": 0,
+            "noOfFemaleGuests": 0,
+            "budget": 0,
+            "serviceInstructions": "none",
+            "status": "REQUEST",
+            "fulfillmentDate": Date.now(),
+            "durationInMinutes": 0,
+            "deliveryType": "Pickup",
+            "order": {
+                "venueNumber": bookedVenueNumber,
+                "orderDate": Date.now(),
+                "orderItems": [
+                    {
+                        "venueNumber": bookedVenueNumber,
+                        "productId": bookedTableId,
+                        "productType": "VenueMap",
+                        "quantity": 5,
+                        "name": "Table 10A"
+                    }
+                ]
+            },
+            "prebooking": false,
+            "employeeName": "",
+            "visitorName": "test test"
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Anonymous " + token
+        }
+    };
+    return options;
+}
